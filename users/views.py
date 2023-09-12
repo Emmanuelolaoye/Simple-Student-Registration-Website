b3308f693043a192eef5f4410c72e4ba3d905a04
emmanue olaoye <eo19181@essex.ac.uk>
2023-09-12 22:17:46 +0100

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from .form import SignUpForm
+#from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignupView(CreateView):
+    #form_class = UserCreationForm
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
+    pass
